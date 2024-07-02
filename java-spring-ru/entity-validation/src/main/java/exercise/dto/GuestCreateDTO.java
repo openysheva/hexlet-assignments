0a56d8c935287861dad780587c3a3f28da6ae837
Yulia Openysheva <openysheva_yulya@mail.ru>
2024-07-02 17:44:32 +0300

    entity validation

---
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -0,0 +1,30 @@
+package exercise.dto;
+
+// BEGIN
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+@Setter
+@Getter
+public class GuestCreateDTO {
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp="(^$|[\\+][0-9]{11,13})")
+    private String phoneNumber;
+
+    @Pattern(regexp="(^$|[0-9]{4})")
+    private String clubCard;
+
+    @Future
+    private LocalDate cardValidUntil;
+}
+// END
